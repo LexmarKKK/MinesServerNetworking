35e0d29589f33bade1a230482d69ddd6bd52d583
Darkar25 <gushi.serega@yandex.ru>
2026-09-22 20:49:46 +0300

    Добавление нового исключения

---
+++ MinesServer.Networking/Exceptions/InvalidPayloadException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MinesServer.Networking.Exceptions;
+
+public class InvalidPayloadException : NetworkException
+{
+    public InvalidPayloadException(string message) : base(message) { }
+
+    public InvalidPayloadException(string message, Exception innerException) : base(message, innerException) { }
+}
